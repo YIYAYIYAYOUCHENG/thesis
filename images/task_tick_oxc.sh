0592b4ca1e7e165eb05747d71277420360dbf65b
syc <syc@syc-laptop.(none)>
2012-08-30 23:53:21 +0200

    first experiment is done

---
+++ images/task_tick_oxc.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/gnuplot
+set terminal png
+set output 'task_tick_oxc.png'
+set title "task_tick_oxc"
+set xtics nomirror rotate by -45 font "0.8"
+#set auto x
+set yrange [0:0.5]
+set xrange [0:7]
+#set style data histogram
+#set style histogram errorbars lw 1
+#set style data linespoints	
+#set bars front
+#plot 'pick_next_task_oxc.dat' using 2:3:xticlabels(1) #with errorbars  
+set style line 1 lc rgb '#0060ad' lt 1 lw 2 pt 7 ps 1.5   # --- blue
+set style line 2 lc rgb '#dd181f' lt 1 lw 2 pt 5 ps 1.5   # --- red
+#set xlabel 'Test index'
+set ylabel 'Time (us)'
+plot 'task_tick_oxc.dat' using 1:2:3:xticlabels(4) title 'execution time' w yerrorbars ls 1
+#, '' using 2:xticlabels(1) w lines
